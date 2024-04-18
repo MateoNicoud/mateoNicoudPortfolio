998ca47e11afe35920824bc02eefb02b36dacde6
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-18 15:52:58 +0200

    parallax added

--- v2/index.js
+++ v2/index.js
@@ -1,6 +1,42 @@
-let images = document.getElementsByClassName('images');
-for (let i = 0; i < images.length; i++) {
-    new simpleParallax(images[i], {
-        scale: 1.5
-    });
+/**
+ *
+ * @param {HTMLElement} element
+ * @return {number}
+ */
+function offsetTop(element, acc =0) {
+    if (element.offsetParent){
+        return offsetTop(element.offsetParent, acc + element.offsetTop);
+    }
+    return acc+element.offsetTop;
 }
+
+
+class Parallax {
+    /**
+     * @param {HTMLElement} element
+     */
+    constructor(element) {
+        this.element = element;
+        this.ratio = parseFloat(element.dataset.parallax);
+        this.onScroll = this.onScroll.bind(this);
+        document.addEventListener('scroll', this.onScroll);
+    }
+    onScroll(){
+        const screenY = window.scrollY+ window.innerHeight/2
+        const elementY = offsetTop(this.element)+ this.element.offsetHeight/2;
+        const diffY = elementY - screenY;
+        this.element.style.setProperty('transform', `translateY(${diffY *-1 *this.ratio}px)`)
+        console.log(elementY - screenY);
+    }
+
+    /**
+     * @returns {Parallax[]}
+     */
+    static bind() {
+        return Array.from(document.querySelectorAll('[data-parallax]')).map((element) => {
+            return new Parallax(element);
+        });
+    }
+}
+
+Parallax.bind();
